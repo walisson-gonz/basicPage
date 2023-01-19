85639c5bc52903d6777ba99ac46b48d73eec277b
walisson-gonz <walisson.gonzales@gmail.com>
2023-01-19 10:45:07 -0300

    progresso 7/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannersComponent } from './body/banners/banners.component';
+import { Cards1Component } from './body/cards1/cards1.component';
+import { Cards2Component } from './body/cards2/cards2.component';
+import { NewsletterComponent } from './body/newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannersComponent,
+    Cards1Component,
+    Cards2Component,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
